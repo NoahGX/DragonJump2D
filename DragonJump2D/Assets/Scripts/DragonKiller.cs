15c022aac9858a9f81ede780fc4b117dd64a06d9
NoahGX <114901822+NoahGX@users.noreply.github.com>
2022-12-10 11:56:32 -0800

    Update DragonKiller.cs

--- DragonJump2D/Assets/Scripts/DragonKiller.cs
+++ DragonJump2D/Assets/Scripts/DragonKiller.cs
@@ -6,9 +6,19 @@ public class DragonKiller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.y > 6 || y < 6)
+        if (transform.position.y > 6)
         {
             SceneManager.LoadScene(0);
         }
+
+        if (transform.position.y < -6)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        SceneManager.LoadScene(0);
     }
 }
