9bdab089b0e29b9be31be80b754feca3545dc3e2
NoahGX <114901822+NoahGX@users.noreply.github.com>
2022-12-10 14:28:56 -0800

    C#

--- DragonJump2D/Assets/Scripts/MoveLeft.cs
+++ DragonJump2D/Assets/Scripts/MoveLeft.cs
@@ -4,6 +4,7 @@ public class MoveLeft : MonoBehaviour
 {
     public float Speed = 5;
 
+
     //Update is called once per frame
     void Update()
     {
@@ -12,6 +13,23 @@ public class MoveLeft : MonoBehaviour
         if (transform.position.x < -12)
         {
             transform.position += Vector3.right * 30;
+
+            ShowRandomSprite();
+        }
+    }
+
+    private void ShowRandomSprite()
+    {
+        int index = UnityEngine.Random.Range(0, 3);
+
+        int childCount = transform.childCount;
+
+        for (int i = 0; i < childCount; i++)
+        {
+            Transform child = transform.GetChild(i);
+            bool shouldShow = index == i;
+
+            child.gameObject.SetActive(shouldShow);
         }
     }
 }
