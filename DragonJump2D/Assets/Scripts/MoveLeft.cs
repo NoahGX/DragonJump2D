f9175aff536b573d2f7c191cbcbd491bf2e1eccc
NoahGX <114901822+NoahGX@users.noreply.github.com>
2022-12-10 12:03:23 -0800

    Update MoveLeft.cs

--- DragonJump2D/Assets/Scripts/MoveLeft.cs
+++ DragonJump2D/Assets/Scripts/MoveLeft.cs
@@ -2,9 +2,11 @@ using UnityEngine;
 
 public class MoveLeft : MonoBehaviour
 {
+    public float Speed = 5;
+
     //Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.left * Time.deltaTime * Speed);
     }
 }
